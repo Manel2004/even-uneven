ea0f66acddfdd4a58634ddbe04e07eefec35d01f
Manel2004 <71586983+Manel2004@users.noreply.github.com>
2022-07-25 10:51:49 +0200

    Update even_uneven do-while.php

--- even_uneven do-while.php
+++ even_uneven do-while.php
@@ -1,12 +1,12 @@
 <?php
-$i = -1;
-$cont = -1;
+$i = 0;
+$cont = 0;
 do {
+    $numeros[] = $i;
     $i++;
-    $numeros[$i] = $i;
 } while ($i <= 100);
 
-for ($n = -1; $n <= 100; $n++) {
+for ($n = 0; $n <= 100; $n++) {
     if ($n % 2 == 0) {
         echo $numeros[$n].PHP_EOL;
     }   
